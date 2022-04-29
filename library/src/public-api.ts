c2903ecd0c9aec9ba5183e19f366414c9c11e6bb
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-04-29 18:50:46 +0200

    test component

--- library/src/public-api.ts
+++ library/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ilpn-components
  */
 
-export * from './lib/ilpn-components.service';
 export * from './lib/ilpn-components.component';
 export * from './lib/ilpn-components.module';
