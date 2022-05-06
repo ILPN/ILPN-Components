1029f49e4dd3864577ea2125d89b65987ec8de36
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-05-06 11:36:00 +0200

    test lib component restored

--- src/lib/ilpn-components.component.ts
+++ src/lib/ilpn-components.component.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'ilpn-ilpn-components',
-    templateUrl: 'ilpn-components.component.html',
-    styleUrls: ['ilpn-components.component.scss']
+    templateUrl: './ilpn-components.component.html',
+    styleUrls: ['./ilpn-components.component.scss']
 })
 export class IlpnComponentsComponent implements OnInit {
 
