578c68a7fb4351384fb4adbbe708d1135845eabe
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-09-06 15:00:59 +0200

    Petri net isomorphism implementation

--- src/lib/algorithms/pn/isomorphism/petri-net-isomorphism.service.ts
+++ src/lib/algorithms/pn/isomorphism/petri-net-isomorphism.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {PetriNet} from '../../../models/pn/model/petri-net';
 import {MapSet} from '../../../utility/map-set';
 import {MappingManager} from './classes/mapping-manager';
+import {Transition} from '../../../models/pn/model/transition';
 
 @Injectable({
     providedIn: 'root'
@@ -39,6 +40,46 @@ export class PetriNetIsomorphismService {
         return false;
     }
 
+    public arePetriNetsIsomorphic(netA: PetriNet, netB: PetriNet): boolean {
+        if (!this.compareBasicNetProperties(netA, netB)) {
+            return false;
+        }
+
+        const transitionMapping = this.determinePossibleTransitionMappings(netA, netB);
+        if (transitionMapping === undefined) {
+            return false;
+        }
+
+        const placeMapping = this.determinePossiblePlaceMappings(netA, netB);
+        if (placeMapping === undefined) {
+            return false;
+        }
+
+        const transitionMappingManager = new MappingManager(transitionMapping);
+        const placeMappingManager = new MappingManager(placeMapping);
+
+        let done = false;
+        do {
+            const transitionMapping = transitionMappingManager.getCurrentMapping();
+            const uniqueTransitionsMapped = new Set<string>(transitionMapping.values());
+            if (transitionMapping.size === uniqueTransitionsMapped.size) { // bijective transition mapping
+                const placeMapping = placeMappingManager.getCurrentMapping();
+                const uniquePlacesMapped = new Set<string>(placeMapping.values());
+                if (placeMapping.size === uniquePlacesMapped.size // bijective place mapping
+                    && this.isMappingAPetriNetIsomorphism(netA, netB, transitionMapping, placeMapping)) {
+                    return true;
+                }
+            }
+
+            const carry = transitionMappingManager.moveToNextMapping();
+            if (carry) {
+                done = placeMappingManager.moveToNextMapping();
+            }
+        } while (!done);
+
+        return false;
+    }
+
     private compareBasicNetProperties(netA: PetriNet, netB: PetriNet): boolean {
         return netA.getTransitionCount() === netB.getTransitionCount()
             && netA.getPlaceCount() === netB.getPlaceCount()
@@ -66,6 +107,25 @@ export class PetriNetIsomorphismService {
         return transitionMapping;
     }
 
+    private determinePossiblePlaceMappings(netA: PetriNet, netB: PetriNet): MapSet<string, string> | undefined {
+        const placeMapping = new MapSet<string, string>();
+        for (const pA of netA.getPlaces()) {
+            let wasMapped = false;
+            for (const pB of netB.getPlaces()) {
+                if (pA.marking === pB.marking
+                    && pA.ingoingArcs.length === pB.ingoingArcs.length
+                    && pA.outgoingArcs.length === pB.outgoingArcs.length) {
+                    wasMapped = true;
+                    placeMapping.add(pA.getId(), pB.getId());
+                }
+            }
+            if (!wasMapped) {
+                return undefined;
+            }
+        }
+        return placeMapping;
+    }
+
     private isMappingAPartialOrderIsomorphism(partialOrderA: PetriNet, partialOrderB: PetriNet, transitionMapping: Map<string, string>): boolean {
         const unmappedArcs = partialOrderB.getPlaces().filter(p => p.ingoingArcs.length !== 0 && p.outgoingArcs.length !== 0);
 
@@ -77,6 +137,31 @@ export class PetriNetIsomorphismService {
             const postTransitionB = transitionMapping.get(arc.outgoingArcs[0].destinationId);
 
             const fittingArcIndex = unmappedArcs.findIndex(unmapped => unmapped.ingoingArcs[0].sourceId === preTransitionB && unmapped.outgoingArcs[0].destinationId === postTransitionB);
+            if (fittingArcIndex === -1) {
+                return false;
+            }
+            unmappedArcs.splice(fittingArcIndex, 1);
+        }
+
+        return true;
+    }
+
+    private isMappingAPetriNetIsomorphism(netA: PetriNet, netB: PetriNet, transitionMapping: Map<string, string>, placeMapping: Map<string, string>): boolean {
+        const unmappedArcs = netB.getArcs();
+
+        for (const arc of netA.getArcs()) {
+            let arcSourceId: string;
+            let arcDestinationId: string;
+            if (arc.source instanceof Transition) {
+                arcSourceId = transitionMapping.get(arc.sourceId)!;
+                arcDestinationId = placeMapping.get(arc.destinationId)!;
+            } else {
+                arcSourceId = placeMapping.get(arc.sourceId)!;
+                arcDestinationId = transitionMapping.get(arc.destinationId)!;
+            }
+
+            // TODO arc weight is not considered when creating possible mappings. Inclusion of this property might make the algorithm more efficient
+            const fittingArcIndex = unmappedArcs.findIndex(unmapped => unmapped.sourceId === arcSourceId && unmapped.destinationId === arcDestinationId && unmapped.weight === arc.weight);
             if (fittingArcIndex === -1) {
                 return false;
             }
