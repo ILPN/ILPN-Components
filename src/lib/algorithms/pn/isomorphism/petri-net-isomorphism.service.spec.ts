f68ea77121b7a5dbb577571854b1a4697e4a649d
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-09-06 13:36:34 +0200

    PO isomorphism tests

--- src/lib/algorithms/pn/isomorphism/petri-net-isomorphism.service.spec.ts
+++ src/lib/algorithms/pn/isomorphism/petri-net-isomorphism.service.spec.ts
@@ -2,13 +2,16 @@ import {TestBed} from '@angular/core/testing';
 import {PetriNetIsomorphismService} from './petri-net-isomorphism.service';
 import {expect} from '@angular/flex-layout/_private-utils/testing';
 import {PetriNet} from '../../../models/pn/model/petri-net';
+import {PetriNetParserService} from '../../../models/pn/parser/petri-net-parser.service';
 
 describe('PetriNetIsomorphismService', () => {
     let isomorphismService: PetriNetIsomorphismService;
+    let parserService: PetriNetParserService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
         isomorphismService = TestBed.inject(PetriNetIsomorphismService);
+        parserService = TestBed.inject(PetriNetParserService);
     });
 
     it('should be created', () => {
@@ -17,5 +20,135 @@ describe('PetriNetIsomorphismService', () => {
 
     it('empty partial orders are isomorphic', () => {
         expect(isomorphismService.arePartialOrderPetriNetsIsomorphic(new PetriNet(), new PetriNet())).toBeTrue();
+    });
+
+    it('A B parallel PO is isomorphic', () => {
+        const po1 = parserService.parse(
+            `.type pn
+.transitions
+a A
+b B
+.places
+p1 0
+p2 0
+p3 0
+p4 0
+.arcs
+p1 a
+a p2
+p3 b
+b p4`
+        );
+        expect(po1).toBeTruthy();
+        const po2 = parserService.parse(
+            `.type pn
+.transitions
+b A
+a B
+.places
+p1 0
+p2 0
+p3 0
+p4 0
+.arcs
+p1 a
+a p2
+p3 b
+b p4`
+        );
+        expect(po2).toBeTruthy();
+        expect(isomorphismService.arePartialOrderPetriNetsIsomorphic(po1!, po2!)).toBeTrue();
+    });
+
+    it('3A sequence PO is isomorphic', () => {
+        const po1 = parserService.parse(
+            `.type pn
+.transitions
+a A
+b A
+c A
+.places
+p1 0
+p2 0
+p3 0
+p4 0
+.arcs
+p1 a
+a p2
+p2 b
+b p3
+p3 c
+c p4`
+        );
+        expect(po1).toBeTruthy();
+        const po2 = parserService.parse(
+            `.type pn
+.transitions
+b A
+a A
+c A
+.places
+p1 0
+p2 0
+p3 0
+p4 0
+.arcs
+p1 a
+a p2
+p2 b
+b p3
+p3 c
+c p4`
+        );
+        expect(po2).toBeTruthy();
+        expect(isomorphismService.arePartialOrderPetriNetsIsomorphic(po1!, po2!)).toBeTrue();
+    });
+
+    it('AB then C is not C then AB PO', () => {
+        const po1 = parserService.parse(
+            `.type pn
+.transitions
+a A
+b B
+c C
+.places
+p1 0
+p2 0
+p3 0
+p4 0
+p5 0
+.arcs
+p1 a
+a p2
+p2 c
+p3 b
+b p4
+p4 c
+c p5`
+        );
+        expect(po1).toBeTruthy();
+        const po2 = parserService.parse(
+            `.type pn
+.transitions
+a A
+b B
+c C
+.places
+p1 0
+p2 0
+p3 0
+p4 0
+p5 0
+.arcs
+p1 c
+c p2
+c p3
+p2 a
+a p4
+p3 b
+b p5`
+        );
+        expect(po2).toBeTruthy();
+        expect(isomorphismService.arePartialOrderPetriNetsIsomorphic(po1!, po2!)).toBeFalse();
     });
 });
