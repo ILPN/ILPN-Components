475d8540e16e137a633ff1d4baa3016366230f3e
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-12-20 10:49:38 +0100

    bad merge

--- src/lib/algorithms/pn/synthesis/prime-miner-configuration.ts
+++
@@ -1,5 +0,0 @@
-import {RegionsConfiguration} from '../regions/classes/regions-configuration';
-
-export interface PrimeMinerConfiguration extends RegionsConfiguration {
-    skipConnectivityCheck?: boolean;
-}
