d87cd4dba115bd12c16dbecdaae3586e8b770111
Jakub Kovář <6195502+minop@users.noreply.github.com>
2023-04-06 15:30:27 +0200

    routingModule and external redirects

--- src/lib/components/public-api.ts
+++ src/lib/components/public-api.ts
@@ -3,3 +3,5 @@ export * from './ilpn-components.module';
 export * from './layout/public-api';
 export * from './interaction/public-api';
 export * from './pn-display/pn-display.component';
+
+export * from './routing/external-redirect-hook.guard';
