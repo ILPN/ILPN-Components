81095671f809f94b50a982ad015b5672d06e1c84
Jakub Kovář <6195502+minop@users.noreply.github.com>
2023-04-19 09:41:11 +0200

    - changed order of svg token elements in relation to their place, so that the tokens don't prevent mouse events on the place

--- src/lib/components/pn-display/svg-net/svg-place.ts
+++ src/lib/components/pn-display/svg-net/svg-place.ts
@@ -31,7 +31,7 @@ export class SvgPlace extends SvgWrapper {
                 textEl.setAttribute('y', `${c.y + parseInt(PLACE_STYLE.r) + this.TEXT_OFFSET}`);
             })
         ];
-        this._elements.push(textEl);
+        this._elements.unshift(textEl);
 
         // text for marking > 9
         this._markingTextEl = this.createTextElement();
@@ -42,40 +42,16 @@ export class SvgPlace extends SvgWrapper {
             })
         );
         this._markingTextEl.setAttribute('font-size', '1.5em');
-        this._elements.push(this._markingTextEl);
+        this._elements.unshift(this._markingTextEl);
 
         // circle tokes
         this._markingTokenEls = [];
         for (const offset of [{x: -1, y: 1}, {x: -1, y: -1}, {x: -1, y: 0}, {x: 0, y: -1}]) {
             for (const flip of [1, -1]) {
-                const token = this.createSvgElement('circle');
-                this.applyStyle(token, TOKEN_STYLE);
-                this._markingTokenEls.push(token);
-                this._placeSubs.push(
-                    this.center$.subscribe(c => {
-                        token.setAttribute('cx', `${c.x + 11 * offset.x * flip}`);
-                        token.setAttribute('cy', `${c.y + 11 * offset.y * flip}`);
-                    })
-                );
-                token.onclick = (_) => {
-                    this.processMouseClick();
-                };
-                this._elements.push(token);
+                this.createTokenSvg(11 * offset.x * flip, 11 * offset.y * flip);
             }
         }
-        const token = this.createSvgElement('circle');
-        this.applyStyle(token, TOKEN_STYLE);
-        this._markingTokenEls.push(token);
-        this._placeSubs.push(
-            this.center$.subscribe(c => {
-                token.setAttribute('cx', `${c.x}`);
-                token.setAttribute('cy', `${c.y}`);
-            })
-        );
-        token.onclick = (_) => {
-            this.processMouseClick();
-        };
-        this._elements.push(token);
+        this.createTokenSvg(0,0);
 
         this.updateMarking(place.marking);
     }
@@ -96,6 +72,19 @@ export class SvgPlace extends SvgWrapper {
 
     public get clicked$(): Observable<string> {
         return this._clicked$.asObservable();
+    }
+
+    private createTokenSvg(cxDelta: number, cyDelta: number) {
+        const token = this.createSvgElement('circle');
+        this.applyStyle(token, TOKEN_STYLE);
+        this._markingTokenEls.push(token);
+        this._placeSubs.push(
+            this.center$.subscribe(c => {
+                token.setAttribute('cx', `${c.x + cxDelta}`);
+                token.setAttribute('cy', `${c.y + cyDelta}`);
+            })
+        );
+        this._elements.unshift(token);
     }
 
     public fill(color: string | undefined) {
