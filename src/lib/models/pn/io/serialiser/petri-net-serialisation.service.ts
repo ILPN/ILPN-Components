51e0c24a50c9efac3febc595a3b91e22f5c8b00b
Jakub Kovář <6195502+minop@users.noreply.github.com>
2025-01-24 11:27:31 +0100

    bugfix

--- src/lib/models/pn/io/serialiser/petri-net-serialisation.service.ts
+++ src/lib/models/pn/io/serialiser/petri-net-serialisation.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {PetriNet} from '../../model/petri-net';
 import {PnOutputFileFormat} from "../model/pn-output-file-format";
 import {PlainPetriNetSerialisationService} from "./plain-petri-net-serialisation.service";
+import {JsonPetriNetSerialisationService} from "./json-petri-net-serialisation.service";
 
 
 @Injectable({
@@ -9,7 +10,8 @@ import {PlainPetriNetSerialisationService} from "./plain-petri-net-serialisation
 })
 export class PetriNetSerialisationService {
 
-    constructor(protected _plainSerialisationService: PlainPetriNetSerialisationService) {
+    constructor(protected _plainSerialisationService: PlainPetriNetSerialisationService,
+                protected _jsonSerialisationService: JsonPetriNetSerialisationService) {
     }
 
     public serialise(net: PetriNet, format: PnOutputFileFormat = PnOutputFileFormat.PLAIN): string {
@@ -17,7 +19,7 @@ export class PetriNetSerialisationService {
             case PnOutputFileFormat.PLAIN:
                 return this._plainSerialisationService.serialise(net);
             case PnOutputFileFormat.JSON:
-                return '';
+                return this._jsonSerialisationService.serialise(net);
             default:
                 throw new Error(`Unsupported output file format '${format}'`);
         }
