38e42aa1df276b2e0b0b51fb4cb4db5ca6159155
Jakub Kovář <6195502+minop@users.noreply.github.com>
2026-01-27 17:01:14 +0100

    FormControl empty value

--- src/lib/utility/glpk/model/regions-configuration.ts
+++ src/lib/utility/glpk/model/regions-configuration.ts
@@ -2,9 +2,9 @@ import {SolverConfiguration} from './solver-configuration';
 
 
 export interface RegionsConfiguration extends SolverConfiguration {
-    noArcWeights?: boolean;
-    noOutputPlaces?: boolean;
-    obtainPartialOrders?: boolean;
+    noArcWeights?: boolean | null;
+    noOutputPlaces?: boolean | null;
+    obtainPartialOrders?: boolean | null;
 
-    logEachRegion?: boolean;
+    logEachRegion?: boolean | null;
 }
